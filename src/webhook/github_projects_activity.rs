c2d08b5cb114b27cf73d0be0ba0f66a14b4354d4
AXON <axonasif@gmail.com>
2023-03-18 14:43:00 +0600

    Second iteration

--- src/webhook/github_projects_activity.rs
+++ src/webhook/github_projects_activity.rs
@@ -31,6 +31,25 @@ use item::{
     ItemNodeOnProjectV2ItemFieldValuesNodes::ProjectV2ItemFieldSingleSelectValue,
 };
 
+use self::project::ProjectNode::ProjectV2;
+use self::project::ProjectNodeOnProjectV2Field::ProjectV2IterationField;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/github.graphql",
+    query_path = "src/queries/project.graphql",
+    response_derives = "Debug"
+)]
+struct Project;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/github.graphql",
+    query_path = "src/queries/add_item_week.graphql",
+    response_derives = "Debug"
+)]
+struct AddItemWeek;
+
 // Receivers
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "rocket::serde")]
@@ -38,7 +57,7 @@ struct GitHubProjectsPayload<'r> {
     action: Cow<'r, str>,
     sender: Sender<'r>,
     changes: Option<ChangesField<'r>>,
-    projects_v2_item: ProjectItem<'r>,
+    projects_v2_item: ProjectStruct<'r>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -55,7 +74,7 @@ struct ChangesField<'r> {
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "rocket::serde")]
-struct ProjectItem<'r> {
+struct ProjectStruct<'r> {
     node_id: Cow<'r, str>,
     project_node_id: Cow<'r, str>,
 }
@@ -77,34 +96,33 @@ async fn github_projects_activity(message: Json<GitHubProjectsPayload<'_>>) -> O
 
     // If the `action` was "edited", check whether any change was made in a field called "Status".
     // If "edited" but the modified field was not "Status", then exit.
-    if action == "edited" {
-        if let Some(value) = &message.changes.as_ref().unwrap().field_node_id {
-            // Variables to pass into graphql
-            let variables = project_field_status::Variables {
-                input_field_node_id: value.to_string(),
-            };
+    if action == "edited" && let Some(value) = &message.changes.as_ref()?.field_node_id {
+        // Variables to pass into graphql
+        let variables = project_field_status::Variables {
+            input_field_node_id: value.to_string(),
+        };
 
-            let project_item_response = post_graphql::<ProjectFieldStatus, _>(
-                github_http_client,
-                &bot_config.github.graphql_api_endpoint,
-                variables,
-            )
-            .await
-            .unwrap()
-            .data?
-            .node?;
+        let project_response = post_graphql::<ProjectFieldStatus, _>(
+            github_http_client,
+            &bot_config.github.graphql_api_endpoint,
+            variables,
+        )
+        .await
+        .ok()?
+        .data?
+        .node?;
 
-            if let ProjectV2SingleSelectField(ref on_match) = project_item_response {
-                if on_match.name != "Status" {
-                    return None;
-                }
+        if let ProjectV2SingleSelectField(ref on_match) = project_response {
+            if on_match.name != "Status" {
+                // Do not continue anymore
+                return None;
             }
         }
     }
 
     // Get the item and status that it changed to
     let variables = item::Variables {
-        input_node_id: node_id.to_string(),
+        input_node_id: node_id.clone(),
     };
 
     let item_response = post_graphql::<Item, _>(
@@ -113,7 +131,7 @@ async fn github_projects_activity(message: Json<GitHubProjectsPayload<'_>>) -> O
         variables,
     )
     .await
-    .unwrap()
+    .ok()?
     .data?
     .node?;
 
@@ -121,16 +139,17 @@ async fn github_projects_activity(message: Json<GitHubProjectsPayload<'_>>) -> O
         // TODO: We could explicitly use ID instead of the "title" (i.e. "name")
         let project_name = Cow::from(item.project.title.to_lowercase());
         let project_name = html_escape::decode_html_entities(&project_name);
+        let item_contents = item.content?;
 
-        let (item_url, item_title) = match item.content? {
-            Issue(object) => (object.url, object.title),
-            PullRequest(object) => (object.url, object.title),
+        let (item_url, item_title) = match &item_contents {
+            Issue(object) => (&object.url, &object.title),
+            PullRequest(object) => (&object.url, &object.title),
             _ => unreachable!(),
         };
         let item_title = html_escape::decode_html_entities(&item_title);
 
         // Set the "Status" for the item in the Project
-        let updated_project_item_status = item
+        let updated_project_status = item
             .field_values
             .nodes
             .into_iter()
@@ -144,14 +163,9 @@ async fn github_projects_activity(message: Json<GitHubProjectsPayload<'_>>) -> O
             })
             .unwrap_or_else(|| "No status".to_owned());
 
-        // IDE team specific tasks
-        if project_name.contains("ide") {
-            let project_node_id = message.projects_v2_item.project_node_id.to_string();
-        }
-
-        let message = match action.as_str() {
+        let slack_message = match action.as_str() {
             "edited" => {
-                format!("<{item_url}|{item_title}> set to *{updated_project_item_status}*")
+                format!("<{item_url}|{item_title}> set to *{updated_project_status}*")
             }
             _ => format!("<{item_url}|{item_title}> added to project"),
         } + &format!(" by <https://githbub.com/{sender_login}|{sender_login}>");
@@ -167,13 +181,13 @@ async fn github_projects_activity(message: Json<GitHubProjectsPayload<'_>>) -> O
                 };
 
                 let payload = json!({
-                  "channel": channel_id.as_ref().unwrap(),
+                  "channel": channel_id.as_ref()?,
                   "blocks": [
                     {
                       "type": "section",
                       "text": {
                         "type": "mrkdwn",
-                        "text": message,
+                        "text": slack_message,
                       }
                     }
                   ]
@@ -185,15 +199,69 @@ async fn github_projects_activity(message: Json<GitHubProjectsPayload<'_>>) -> O
                     .json(&payload)
                     .send()
                     .await
-                    .unwrap()
+                    .ok()?
                     .text()
                     .await
-                    .unwrap();
+                    .ok()?;
+            }
+        }
+
+        // IDE team specific tasks
+        if project_name.contains("ide") && let Issue(content) = &item_contents && let Some(labels) = &content.labels {
+            let is_epic = labels
+                .nodes
+                .as_ref()
+                .into_iter()
+                .flatten()
+                .filter_map(|node| node.as_ref())
+                .any(|label| label.name == "type: epic");
+
+            let project_node_id = message.projects_v2_item.project_node_id.to_string();
+
+            // If "Week" field is not set and the item is not labled with "type: epic"
+            if item.field_value_by_name.is_none() && !is_epic {
+                // Get the current possible iteration IDs from Project config/settings
+                let variables = project::Variables {
+                    input_project_node_id: project_node_id.clone(),
+                };
+
+                let project_response = post_graphql::<Project, _>(
+                    github_http_client,
+                    &bot_config.github.graphql_api_endpoint,
+                    variables,
+                )
+                .await
+                .ok()?
+                .data?
+                .node?;
+
+                // Set "Week" iteration value
+                // This sets the next (upcoming) week if exists or the current one otherwise.
+                if let ProjectV2(project) = project_response
+                && let Some(ProjectV2IterationField(field)) = project.field
+                && let Some(iteration) = field.configuration.iterations.get(1).or_else(|| field.configuration.iterations.get(0)) {
+                    
+                    // Get the current possible iteration IDs from Project config/settings
+                    let variables = add_item_week::Variables {
+                       input_node_id: node_id, 
+                        input_project_node_id: project_node_id,
+                        input_field_id: field.id,
+                        input_iterations_id: iteration.id.clone(),
+                    };
+
+                    post_graphql::<AddItemWeek, _>(
+                        github_http_client,
+                        &bot_config.github.graphql_api_endpoint,
+                        variables,
+                    )
+                    .await
+                    .ok()?;
+                }
             }
         }
     }
 
-    Some(json!({ "status": "ok", "sender_login": sender_login }))
+    Some(json!({ "status": "ok" }))
 }
 
 #[catch(404)]
